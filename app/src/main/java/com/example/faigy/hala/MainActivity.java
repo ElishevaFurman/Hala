efb66af766ada835ba76ebba7841fed65122a062
rsorotzkin <rsorotzkin@gmail.com>
2016-01-21 12:56:30 -0500

    merge

--- app/src/main/java/com/example/faigy/hala/MainActivity.java
+++ app/src/main/java/com/example/faigy/hala/MainActivity.java
@@ -80,17 +80,25 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_team) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_services) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_news) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_testimonials) {
+
+        } else if (id == R.id.nav_grow) {
+
+        } else if (id == R.id.nav_donate) {
+
+        } else if (id == R.id.nav_contact) {
+
+        } else if (id == R.id.nav_faqs) {
 
         }
 
