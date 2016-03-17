c292081c5cfb24821c24407c11cf906d88cb4770
ElishevaFurman <le7friedman@gmail.com>
2016-03-17 13:29:32 -0400

    update

--- app/src/main/java/com/example/faigy/hala/Adapters/TeamMemberAdapter.java
+++ app/src/main/java/com/example/faigy/hala/Adapters/TeamMemberAdapter.java
@@ -2,9 +2,12 @@ package com.example.faigy.hala.Adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -76,26 +79,38 @@ public class TeamMemberAdapter extends RecyclerView.Adapter<TeamMemberAdapter.Te
         // get data from your newsList at this position
         // replace the contents of the view with that newsList data
         tm = teamMembersList.get(position);
+        // Set name for each textView
         teamMemberViewHolder.vName.setText(tm.title + " " + tm.name);
-        teamMemberViewHolder.vTitle.setText(tm.description);
-        teamMemberViewHolder.vBio.setText(tm.bio);
+        // Set description for each textView
+        teamMemberViewHolder.vDescription.setText(tm.description);
+        // set content to string for the bio text
+        String content = String.valueOf(Html
+                .fromHtml("<![CDATA[<body style=\"text-align:justify;\">"
+                        + tm.bio.replace("\n", "<br /> <br />") + "</body>]]>"));
+        // Load content into bio webView as text
+        teamMemberViewHolder.bio.loadData(content, "text/html", "utf-8");
+        // Instantiate webViewSettings
+        WebSettings webSettings = teamMemberViewHolder.bio.getSettings();
+        // Set font size to text in webView
+        webSettings.setDefaultFontSize(12);
+        // Load member image from url into imageView
         Picasso.with(context).load("http://" + tm.getImage())
                 .transform(new CircleTransform()).into(teamMemberViewHolder.teamMemberImageView
                 , new Callback() {
                     @Override
                     public void onSuccess() {
-                        // TODO Auto-generated method stub
+                        //Remove progressBar on success of loaded image
                         teamMemberViewHolder.imageProgressBar.setVisibility(View.GONE);
                     }
 
                     @Override
                     public void onError() {
-                        // TODO Auto-generated method stub
+                        // Remove progressBar on error of loaded image
                         teamMemberViewHolder.imageProgressBar.setVisibility(View.GONE);
+                        // Set the image to default
                         teamMemberViewHolder.teamMemberImageView.setBackgroundResource(R.mipmap.ic_launcher_hala);
                     }
                 });
-
 
 
         // if position is equal to expanded position
@@ -129,7 +144,8 @@ public class TeamMemberAdapter extends RecyclerView.Adapter<TeamMemberAdapter.Te
 
     // this will store the references to our view
     public class TeamMemberViewHolder extends RecyclerView.ViewHolder {
-        protected TextView vName,  vTitle, vBio;
+        protected TextView vName,  vDescription;
+        public WebView bio;
         public LinearLayout expandLinearLayout;
         public ImageButton expandArrow;
         public ImageView teamMemberImageView;
@@ -137,9 +153,10 @@ public class TeamMemberAdapter extends RecyclerView.Adapter<TeamMemberAdapter.Te
 
         public TeamMemberViewHolder(View v) {
             super(v);
+            // Initialize all views in viewHolder
             vName = (TextView) v.findViewById(R.id.txtName);
-            vTitle = (TextView) v.findViewById(R.id.title);
-            vBio = (TextView) v.findViewById(R.id.bioTextView);
+            vDescription = (TextView) v.findViewById(R.id.title);
+            bio = (WebView) v.findViewById(R.id.bioWebView);
             expandLinearLayout = (LinearLayout) v.findViewById(R.id.expandLinearLayout);
             expandArrow = (ImageButton) v.findViewById(R.id.expandArrow);
             teamMemberImageView = (ImageView) v.findViewById(R.id.teamMemberImageView);
